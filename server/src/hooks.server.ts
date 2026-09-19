8454aff15945182cf723361d1a8c41e26cf59b52
LunaeSomnia <adrianmorales.t@proton.me>
2026-09-19 09:37:56 +0200

    chore: i'm starting over
    lets rethink this

--- server/src/hooks.server.ts
+++
@@ -1,4 +0,0 @@
-import { initDb } from "$lib/db.svelte";
-import type { Handle } from "@sveltejs/kit";
-
-initDb();
--- server/src/lib/model.ts
+++
@@ -1,4 +0,0 @@
-export interface TemperatureData {
-    timestamp: number;
-    data: number;
-}
